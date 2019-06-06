5c6339d3c92d6b94ee8ba3d83284d1089460945e
lym1993 <43030015+lym1993@users.noreply.github.com>
2019-06-06 10:52:18 +0800

    Revert "5.31"
    This reverts commit 32ceca8b8a4993bf1a57d1d000949862e6eb0dd0.

--- Chenjiangcesuan/Chenjiangcesuan/MainWindow.xaml.cs
+++ Chenjiangcesuan/Chenjiangcesuan/MainWindow.xaml.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -21,75 +20,12 @@ namespace Chenjiangcesuan
     /// </summary>
     public partial class MainWindow : Window
     {
-        ObservableCollection<Soilinformation> soilinformationsItems = new
-            ObservableCollection<Soilinformation>(); //实例化动态数据集合
-
         public MainWindow()
         {
             InitializeComponent();
             
-            GetComboBoxSource();
-            datagrid1.ItemsSource = soilinformationsItems;
         }
-
-        public void AddRow()
-        {
-            Soilinformation soilinformation = new Soilinformation();
-
-            soilinformation.SoilThickness = 0;
-            soilinformation.SoilUnitWeight = 0;
-            soilinformation.Voidratio0kPa = 0;
-            soilinformation.Voidratio50kPa = 0;
-            soilinformation.Voidratio100kPa = 0;
-            soilinformation.Voidratio200kPa = 0;
-            soilinformation.Voidratio300kPa = 0;
-            soilinformation.Voidratio400kPa = 0;
-
-            soilinformationsItems.Add(soilinformation);
-        }
-        //添加土层按钮
-        private void btnAdd_Click(object sender, RoutedEventArgs e)
-        {
-            AddRow();
-        }
-
-        //删除土层按钮
-        private void btnDel_Click(object sender, RoutedEventArgs e)
-        {
-            if(datagrid1.SelectedItems!=null&datagrid1.SelectedItems.Count>0)
-            {
-                for(int i=datagrid1.SelectedItems.Count-1;i>=0; i--)
-                {
-                    soilinformationsItems.Remove(datagrid1.SelectedItems[i] as Soilinformation);
-                }
-            }
-        }
-       
-        private void GetComboBoxSource()
-        {
-            cbbSelectMode.Items.Add(DataGridSelectionUnit.Cell);
-            cbbSelectMode.Items.Add(DataGridSelectionUnit.FullRow);
-            cbbSelectMode.Items.Add(DataGridSelectionUnit.CellOrRowHeader);
-            cbbSelectMode.SelectedIndex = 2;
-        }
-
-        //自动添加行号
-        private void Datagrid1_LoadingRow(object sender, DataGridRowEventArgs e)
-        {
-            e.Row.Header = e.Row.GetIndex() + 1;
-        }
-
-        //Enter达到tab效果
-        private void Datagrid1_PreviewKeyDown(object sender, KeyEventArgs e)
-        {
-            var uie = e.OriginalSource as UIElement;
-            if (e.Key==Key.Enter)
-            {
-                uie.MoveFocus(new TraversalRequest(FocusNavigationDirection.Next));
-                e.Handled = true;
-            }
-        }
-            
+              
         //荷载
         private double Load
         {
@@ -122,17 +58,12 @@ namespace Chenjiangcesuan
         }
 
         
-        //combox改变选择事件
-        private void CbbSelectMode_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            datagrid1.SelectionUnit = (DataGridSelectionUnit)cbbSelectMode.SelectedValue;
-        }
-       
-        //clear按钮事件
+            
+
+        
         private void ClearBtn_Click(object sender, RoutedEventArgs e)
         {
-            
+           
         }
-        
     }
 }
